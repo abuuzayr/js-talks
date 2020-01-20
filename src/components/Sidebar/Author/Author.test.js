860537699c919d412c21ffccb51e904eb4042559
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-01-20 17:41:33 +0800

    Update new logo tests

--- src/components/Sidebar/Author/Author.test.js
+++ src/components/Sidebar/Author/Author.test.js
@@ -7,7 +7,7 @@ describe('Author', () => {
   const props = {
     author: {
       name: 'test',
-      photo: '/photo.jpg',
+      photo: '/js-logo.png',
       bio: 'test'
     },
     isIndex: false
