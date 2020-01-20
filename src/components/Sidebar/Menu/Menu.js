4ba6a2a0d61812c869312d0b608ca67a848d4269
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-01-20 17:41:06 +0800

    Update menu links to handle external paths

--- src/components/Sidebar/Menu/Menu.js
+++ src/components/Sidebar/Menu/Menu.js
@@ -15,13 +15,23 @@ const Menu = ({ menu }: Props) => (
     <ul className={styles['menu__list']}>
       {menu.map((item) => (
         <li className={styles['menu__list-item']} key={item.path}>
-          <Link
-            to={item.path}
-            className={styles['menu__list-item-link']}
-            activeClassName={styles['menu__list-item-link--active']}
-          >
-            {item.label}
-          </Link>
+          {
+            item.path.match(/^(http|mailto)/) ?
+            <a 
+              href={item.path}
+              target="_blank"
+              className={styles['menu__list-item-link']}
+            >
+              {item.label}
+            </a>
+            : <Link
+                to={item.path}
+                className={styles['menu__list-item-link']}
+                activeClassName={styles['menu__list-item-link--active']}
+              >
+                {item.label}
+              </Link>
+          }
         </li>
       ))}
     </ul>
