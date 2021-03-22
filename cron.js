199fb87b6918ee614717eca053c2dfbd1f5cdbc2
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-03-23 04:44:46 +0800

    Add yt download

--- cron.js
+++ cron.js
@@ -1,9 +1,30 @@
-const job = (token, episodes) => {
-    console.log(token)
-    console.log(episodes)
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
+const job = async (token, episodes) => {
+    // 2. check json for dates that are past and are not uploaded yet
+    // 3. select an episode
+    const episode = episodes.find(e => {
+        return new Date(e.posting_date) < new Date() && !e.b2_url
+    })
+    ytdl(episode.yt_url, {
+        quality: 'highestaudio',
+        filter: 'audioonly'
+    }).pipe(fs.createWriteStream(`${episode.slug}.mp3`))
+    let stats = fs.statSync(`${episode.slug}.mp3`)
+    let fileSizeInBytes = stats.size;
+    console.log("fileSizeInBytes: ", fileSizeInBytes)
+    // 4. download the audio from youtube
+    // 5. upload the audio to b2
+    // 6. update the json
+    // --- b2_url
+    // 7. create the post md file
+    // 8. update readme
+    // 9. push the repo
+    // 10. trigger build
 }
 
 module.exports = async (token, episodes) => {
-    job(token, episodes)
-    return context.payload.client_payload.value
+    await job(token, episodes)
+    return
 }
